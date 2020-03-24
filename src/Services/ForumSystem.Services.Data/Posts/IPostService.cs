ca5af83893a98c42a45b69b59e79a00a37eb2c89
Dimitar Samarov <samarovdimitar@gmail.com>
2020-03-25 00:04:10 +0200

    Posts service added!

---
+++ src/Services/ForumSystem.Services.Data/Posts/IPostService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumSystem.Services.Data.Posts
+{
+    using System.Threading.Tasks;
+    using ForumSystem.Data.Models;
+
+    public interface IPostService
+    {
+        Task<IEnumerable<T>> GetAllFromCategory<T>(int categoryId)
+            where T : class;
+
+        Task CreatePost(string title, string imageUrl, string content);
+
+        Task RemovePost(int id);
+
+        Task<T> GetByIdAsync<T>(int id);
+    }
+}
---
+++ src/Services/ForumSystem.Services.Data/Posts/PostsService.cs
@@ -0,0 +1,59 @@
+﻿namespace ForumSystem.Services.Data.Posts
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using ForumSystem.Data.Common.Repositories;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Extensions;
+    using Mapping;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PostsService : IPostService
+    {
+        private readonly IDeletableEntityRepository<Post> postsRepository;
+
+        public PostsService(IDeletableEntityRepository<Post> postsRepository)
+        {
+            this.postsRepository = postsRepository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllFromCategory<T>(int categoryId)
+        where T : class
+        {
+            return await this.postsRepository.All()
+                .Where(x => x.Category.Id == categoryId)
+                .To<T>()
+                .IncludeAll()
+                .ToListAsync();
+        }
+
+        public async Task CreatePost(string title, string imageUrl, string content)
+        {
+            var post = new Post
+            {
+                Title = title,
+                ImageUrl = imageUrl,
+                Content = content,
+            };
+
+            await this.postsRepository.AddAsync(post);
+            await this.postsRepository.SaveChangesAsync();
+        }
+
+        public async Task RemovePost(int id)
+        {
+            var post = await this.GetByIdAsync<Post>(id);
+
+            this.postsRepository.Delete(post);
+            await this.postsRepository.SaveChangesAsync();
+        }
+
+        public async Task<T> GetByIdAsync<T>(int id)
+        {
+            var obj = await this.postsRepository.All().Where(x => x.Id == id).To<T>().FirstOrDefaultAsync();
+
+            return obj;
+        }
+    }
+}
