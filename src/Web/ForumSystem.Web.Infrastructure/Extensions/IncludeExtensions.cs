bd2905e221ada3fb0c6bae1327414cf469e27e93
Dimitar Samarov <samarovdimitar@gmail.com>
2020-03-25 00:06:19 +0200

    Some extensions added

---
+++ src/Web/ForumSystem.Web.Infrastructure/Extensions/IncludeExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace ForumSystem.Web.Infrastructure.Extensions
+{
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class IncludeExtensions
+    {
+        public static IQueryable<T> IncludeAll<T>(this IQueryable<T> queryable)
+            where T : class
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties();
+            foreach (var property in properties)
+            {
+                var isVirtual = property.GetGetMethod().IsVirtual;
+                if (isVirtual && properties.FirstOrDefault(c => c.Name == property.Name + "Id") != null)
+                {
+                    queryable = queryable.Include(property.Name);
+                }
+            }
+
+            return queryable;
+
+        }
+    }
+}
